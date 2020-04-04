f0f95e221f559d39eab028ccad5e274809a9c0de
pikalover6 <49179590+pikalover6@users.noreply.github.com>
2020-04-04 11:02:39 +0200

    Made script much faster. Yay!

--- install.sh
+++ install.sh
@@ -4,13 +4,12 @@ echo 'This script will install the latest version of wine to allow you to instal
 sudo dpkg --add-architecture i386 
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
-CODENAME=lsb_release --codename | cut -f2
+CODENAME=$(lsb_release --codename | cut -f2)
+print 'Codename found: '$CODENAME.
 echo 'Adding repositories...'
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/' + $CODENAME + ' main'
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ $CODENAME main'
 echo 'Updating...'
 sudo apt update
-echo 'Installing package winehq-stable.'
+echo 'Installing package winehq-stable.' 
 sudo apt install --install-recommends winehq-stable
 echo 'Done installing, you may now execute windows apps. Thank you for using this script.'
-
-
